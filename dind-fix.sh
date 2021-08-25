3b109e8cf69d99eb4827a8aab109534b04351d9e
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2021-08-25 07:38:51 +0200

    Create dind-fix.sh

---
+++ dind-fix.sh
@@ -0,0 +1,10 @@
+cat /etc/docker/daemon.json <<EOF
+{
+    "debug": true,
+    "registry-mirrors": ["https://mirror.gcr.io"],
+    "insecure-registries": ["https://mirror.gcr.io"]
+}
+EOF
+
+
+systemctl restart docker
